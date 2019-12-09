b4af4cfaad753a938eae6e24dd2189aed6e08106
the-suraj <surajsjjha@gmail.com>
2019-12-10 01:48:00 +0530

    compatible with dark mode

--- ui_scripts/style.js
+++ ui_scripts/style.js
@@ -4,7 +4,7 @@ let delete_Input = (event) => {
 }
 
 let add_new_input = () => {
-    add_input_btn.parentElement.insertAdjacentHTML('beforebegin', '<label for="equationInputBox" class="equation_in_label"><input type="text" class="equationInputBox equation_in"  placeholder="Your Equation"><button class="delete_input"><svg viewBox="0 0 348.333 348.334"><path d="M336.559,68.611L231.016,174.165l105.543,105.549c15.699,15.705,15.699,41.145,0,56.85c-7.844,7.844-18.128,11.769-28.407,11.769c-10.296,0-20.581-3.919-28.419-11.769L174.167,231.003L68.609,336.563c-7.843,7.844-18.128,11.769-28.416,11.769c-10.285,0-20.563-3.919-28.413-11.769c-15.699-15.698-15.699-41.139,0-56.85l105.54-105.549L11.774,68.611c-15.699-15.699-15.699-41.145,0-56.844c15.696-15.687,41.127-15.687,56.829,0l105.563,105.554L279.721,11.767c15.705-15.687,41.139-15.687,56.832,0C352.258,27.466,352.258,52.912,336.559,68.611z"/></svg></button></label>');
+    add_input_btn.parentElement.insertAdjacentHTML('beforebegin', '<label for="equationInputBox" class="equation_in_label"><input type="text" class="equationInputBox equation_in"  placeholder="Write Equation..."><button class="delete_input"><svg viewBox="0 0 348.333 348.334"><path d="M336.559,68.611L231.016,174.165l105.543,105.549c15.699,15.705,15.699,41.145,0,56.85c-7.844,7.844-18.128,11.769-28.407,11.769c-10.296,0-20.581-3.919-28.419-11.769L174.167,231.003L68.609,336.563c-7.843,7.844-18.128,11.769-28.416,11.769c-10.285,0-20.563-3.919-28.413-11.769c-15.699-15.698-15.699-41.139,0-56.85l105.54-105.549L11.774,68.611c-15.699-15.699-15.699-41.145,0-56.844c15.696-15.687,41.127-15.687,56.829,0l105.563,105.554L279.721,11.767c15.705-15.687,41.139-15.687,56.832,0C352.258,27.466,352.258,52.912,336.559,68.611z"/></svg></button></label>');
     let new_in_label = add_input_btn.parentElement.previousSibling;
     new_in_label.children[0].focus();
     new_in_label.children[1].addEventListener('click', () => {
@@ -19,6 +19,7 @@ let set_layout = () => {
     console.log(window.height)
     output.style.height = `calc(${window.innerHeight - document.getElementById('body-header').offsetHeight}px - 2em)`;
     outputContainer.style.height = `${window.innerHeight - document.getElementById('body-header').offsetHeight}px`;
+    document.body.style.minHeight = window.innerHeight + 'px';
 }
 window.addEventListener('resize', set_layout);
 set_layout();

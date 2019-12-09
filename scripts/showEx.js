61ff3e0b540f8d6e5a575f1392d681688cc91cc5
the-suraj <surajsjjha@gmail.com>
2019-12-09 07:07:38 +0530

    compatible with auto input bar

--- scripts/showEx.js
+++ scripts/showEx.js
@@ -1,21 +1,20 @@
 let Example = () => {
     let exampleArr = [
         ['x+2y+3z=3', '2x+4y+5z=5', '3x+5y+6z= -7'], //x = -26, y = 13, z = 1
-        ['x+y+z=9', '2x+5y+7z=52', '2x+y-z=0'], // x = 1, y = 3, z = 5
-        ['x-y+z=0', '0x+y+z=1', '3x+2y-4z=3'], // x = 2/3, y = 5/6, z = 1/6
-        ['x+y+z=3','2x-y+3z=4','3x+4z=7'], // infinite solutions
-        ['2x+y=5', '4x+y=8'], // x = 3/2, y = 2
-        ['2x - y + z = -1', 'x + 2y + 3z = -8', '3x + y - 4z = -1'], // x = -1, y = -2, z = -1
-        ['6x=6+9+3x'], // x = 5
-        ['3x+2y+6z=20', 'x+y+2z=3', '2x+2y-5z=6'] // x = 14, y = -11, z = 0
+        // ['x+y+z=9', '2x+5y+7z=52', '2x+y-z=0'], // x = 1, y = 3, z = 5
+        // ['x-y+z=0', '0x+y+z=1', '3x+2y-4z=3'], // x = 2/3, y = 5/6, z = 1/6
+        // ['x+y+z=3','2x-y+3z=4','3x+4z=7'], // infinite solutions
+        // ['2x+y=5', '4x+y=8'], // x = 3/2, y = 2
+        // ['2x - y + z = -1', 'x + 2y + 3z = -8', '3x + y - 4z = -1'], // x = -1, y = -2, z = -1
+        // ['6x=6+9+3x'], // x = 5
+        // ['3x+2y+6z=20', 'x+y+2z=3', '2x+2y-5z=6'] // x = 14, y = -11, z = 0
     ];
     setTimeout(() => {
         setExample(exampleArr[parseInt(Math.random() * 10) % exampleArr.length]);
     }, 200);
 }
 let setExample = (exampleEquation) => {
-    // let exampleEquation = ['x+2y+3z=3', '2x+4y+5z=5', '3x+5y+6z=7'];
-    alert(`Lets solve the Equations ${exampleEquation}`);
+    // alert(`Lets solve the Equations ${exampleEquation}`);
     setTimeout(() => {
          for (let i = 0; i < exampleEquation.length-1; i++) {
              add_input_bar();
